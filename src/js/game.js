e23f1f885c0034fa6ccbf60bff6290d2cf758ed3
Ryan Nicolau Lopes <90126273+KauanNLopes@users.noreply.github.com>
2021-09-05 03:35:04 -0300

    add jogo

---
+++ src/js/game.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(1000,650,Phaser.CANVAS);
+
+ game.state.add('boot',bootState);
+ game.state.add('load',loadState);
+ game.state.add('menu',menuState);
+ game.state.add('stage1',stage1State);
+ game.state.add('stage2',stage2State);
+ game.state.add('stage3',stage3State);
+ game.state.add('stage4',stage4State);
+ game.state.add('stage5',stage5State);
+ game.state.add('end',endState);
+
+ game.state.start('boot');
